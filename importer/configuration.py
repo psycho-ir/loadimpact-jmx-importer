418905d13cc1fe854b505bfba9b855dfcca9f665
soroosh <soroosh.sarabadani@gmail.com>
2014-11-22 15:39:22 +0330

    when http does not exist it will be added to url

--- importer/configuration.py
+++ importer/configuration.py
@@ -4,23 +4,29 @@ import settings
 
 __all__ = ['ConfigurationGenerator', 'ConfigurationUploader']
 
+
 class ConfigurationGenerator(object):
     def __init__(self, jmx_info):
         self.jmx_info = jmx_info
 
     def generate_configuration(self, scenario):
+        domain = self.jmx_info.domain
+        if not domain.startswith("http://"):
+            domain = 'http://' + domain
+
+        duration = self.jmx_info.ramp_time / 60
         config = {'name': self.jmx_info.test_name,
-                  'url': 'http://' + self.jmx_info.domain,
+                  'url': domain,
                   'config': {
                       'load_schedule': [
-                          {'users': self.jmx_info.num_of_threads, 'duration': self.jmx_info.ramp_time / 60}],
+                          {'users': self.jmx_info.num_of_threads, 'duration': duration}],
                       'tracks': [{
                                      'clips': [{
                                                    'user_scenario_id': scenario.id, 'percent': 100
                                                }],
                                      'loadzone': settings.TIME_ZONE
                                  }],
-                      'user_type': 'sbu'
+                      'user_type': settings.USER_TYPE
                   }
         }
 
