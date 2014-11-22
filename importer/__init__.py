ae12d6aafaa31850a8d411e90e5c471444180a4e
soroosh <soroosh.sarabadani@gmail.com>
2014-11-22 15:09:34 +0330

    SimpleAPI added to project and integration tests improved

--- importer/__init__.py
+++ importer/__init__.py
@@ -1,1 +1,36 @@
+from importer.jmx_reader import JMXInfo, JMXReader
+
 __author__ = 'soroosh'
+
+from configuration import *
+from scenario import *
+
+
+class SimpleAPI(object):
+    def upload_jmx_files(self, file_paths, fail_fast=True):
+        file_paths = set(file_paths)
+        failed_paths = list()
+        for file_path in file_paths:
+            try:
+                reader = JMXReader(file_path)
+                jmx_info = reader.create_jmx_info()
+                print 'JMX Information has been read. JMX Info: %s' % jmx_info
+                generator = ScenarioGenerator(jmx_info)
+                script = generator.generate_scenario()
+                print 'Created script is: %s' % script
+                scenario = ScenarioUploader.upload(jmx_info.test_name, script)
+                print 'Scenario uploaded with id:%s' % scenario.id
+                config_generator = ConfigurationGenerator(jmx_info)
+                data = config_generator.generate_configuration(scenario)
+                print 'Created configuration test is: %s' % data
+                config = ConfigurationUploader.upload(data)
+                print 'Test configuration uploaded with id:%s' % config.id
+            except Exception as e:
+                print "An error occured on uploading jmx file: %s, Error message: %s" % (file_path, e)
+                failed_paths.append((file_path, e))
+                if fail_fast:
+                    return failed_paths
+
+        return failed_paths
+
+
--- test/integration_test.py
+++ test/integration_test.py
@@ -1,3 +1,4 @@
+from importer import SimpleAPI
 import settings
 
 __author__ = 'soroosh'
@@ -9,17 +10,18 @@ from importer.configuration import *
 import loadimpact.clients
 
 
-
-class ScenarioIntegrationTest(unittest.TestCase):
+class IntegrationTest(unittest.TestCase):
     def setUp(self):
+        print "Cleaning..."
         client = loadimpact.ApiTokenClient(api_token=settings.loadimpact_api_token)
         configs = client.list_test_configs()
         map(lambda config: config.delete(), configs)
         scenarios = client.list_user_scenarios()
-        scenario = filter(lambda s: s.name == 'test3', scenarios)
-        if scenario:
-            scenario[0].delete()
+        for scenario in scenarios:
+            scenario.delete()
 
+
+class ScenarioIntegrationTest(IntegrationTest):
     def test_scenario_creation(self):
         reader = JMXReader('../plans/Jmetertestplan.jmx')
         jmx_info = reader.create_jmx_info()
@@ -29,6 +31,8 @@ class ScenarioIntegrationTest(unittest.TestCase):
         scenario = ScenarioUploader.upload('test3', script)
         print scenario
 
+
+class ConfigurationIntegrationTest(IntegrationTest):
     def test_configuration_creation(self):
         reader = JMXReader('../plans/Jmetertestplan.jmx')
         jmx_info = reader.create_jmx_info()
@@ -38,6 +42,32 @@ class ScenarioIntegrationTest(unittest.TestCase):
         scenario = ScenarioUploader.upload('test3', script)
         config_generator = ConfigurationGenerator(jmx_info)
         data = config_generator.generate_configuration(scenario)
-        print 'Generate dats is: %s' % data
+        print 'Generate data is: %s' % data
         config = ConfigurationUploader.upload(data)
         print config
+
+
+class SimpleAPITest(IntegrationTest):
+    def test_simpleapi_with_one_jmx(self):
+        api = SimpleAPI()
+        paths = ['../plans/Jmetertestplan.jmx', '../plans/Jmetertestplan.jmx']
+        api.upload_jmx_files(paths)
+        print type(paths)
+
+    def test_upload_should_return_one_failed_path(self):
+        api = SimpleAPI()
+        paths = ['..', '../plans/Jmetertestplan.jmx']
+        failed_result = api.upload_jmx_files(paths)
+        self.assertEqual(1, len(failed_result))
+
+    def test_upload_should_return_one_failed_when_is_fail_fast_with_more_than_one_error(self):
+        api = SimpleAPI()
+        paths = ['..', '../plans/Jmetertestplan.jmx', '/', '...']
+        failed_result = api.upload_jmx_files(paths)
+        self.assertEqual(1, len(failed_result))
+
+    def test_upload_should_return_all_failed_paths_when_is_not_fail_fast(self):
+        api = SimpleAPI()
+        paths = ['..', '../plans/Jmetertestplan.jmx', '/', '...']
+        failed_result = api.upload_jmx_files(paths, fail_fast=False)
+        self.assertEqual(3, len(failed_result))
